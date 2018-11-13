cdf0312236776350196cbcd49e13c7664e0457d7
Nicholas Killewald <captainspam@gmail.com>
2018-11-12 23:49:56 -0800

    Added warning message for Omega Shift.
    Since this now makes network calls and it might be a bit late to the
    party, I figured people might want to know.

--- app/src/main/java/net/exclaimindustries/dbshiftwallpaper/DBWallpaperPreferences.java
+++ app/src/main/java/net/exclaimindustries/dbshiftwallpaper/DBWallpaperPreferences.java
@@ -1,25 +1,41 @@
 package net.exclaimindustries.dbshiftwallpaper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 
 /**
- * This sets up the preferences for the wallpaper.  There is exactly one
- * preference right now.
+ * This sets up the preferences for the wallpaper.
  */
 public class DBWallpaperPreferences extends Activity {
     public static class DBPrefsFragment extends PreferenceFragment {
         public DBPrefsFragment() {}
 
+        private static final String OMEGA_DIALOG = "OmegaDialog";
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_main);
+
+            // If the user sets Omega Shift to true, throw the warning first.
+            Preference omega = findPreference(DBWallpaperService.PREF_OMEGASHIFT);
+            omega.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    if(newValue.equals(Boolean.TRUE)) {
+                        new OmegaReminderDialog().show(getFragmentManager(), OMEGA_DIALOG);
+                    }
+
+                    return true;
+                }
+            });
         }
     }
 
@@ -31,5 +47,21 @@ public class DBWallpaperPreferences extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new DBPrefsFragment())
                 .commit();
+    }
+
+    public static class OmegaReminderDialog extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder build = new AlertDialog.Builder(getActivity())
+                .setMessage(R.string.omega_warning)
+                .setPositiveButton(R.string.thats_fair, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+            return build.create();
+        }
     }
 }
